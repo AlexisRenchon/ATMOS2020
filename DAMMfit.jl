3faf35d774f432c6b9d6525dacf21198d8a1d57f
AlexisRenchon <a.renchon@gmail.com>
2021-08-23 12:54:46 -0500

    all 64 locations

--- DAMMfit.jl
+++ DAMMfit.jl
@@ -1,92 +1,86 @@
-using DataFrames, CSV
+using DataFrames, CSV, LsqFit
 
 data = DataFrame(CSV.File("Input/2020_v1.csv"))
 
 # using DAMMmodel
 include("DAMM_scaled_porosity.jl")
 
-# get dataframe of RSM_Exp_Flux_00
-
-d = dropmissing(data, :RSM_Exp_Flux_03)
-# d = dropmissing(data, :RSA_Exp_Flux_1)
-
-SWC = d.SWC_03
-Tsoil = d.Tsoil_03
-
-Rsoil = Dep_var = d.RSM_Exp_Flux_03
-# Rsoil = Dep_var = d.RSA_Exp_Flux_1
-
-Ind_var = hcat(Tsoil, SWC)
-
-poro_val = maximum(skipmissing(data.SWC_03))
+# to do: loop to DAMM get parameters for all of 64 locations
+x = [0,0,0,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,0,0,0,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7] 
+y = [0,1,2,2,1,0,0,1,2,2,1,0,0,1,2,2,1,0,0,1,2,2,1,0,3,4,4,3,4,3,3,4,4,3,3,4,4,3,4,3,5,6,7,7,6,5,5,6,7,7,6,5,5,6,7,7,6,5,5,6,7,7,6,5] 
 
 lb = [0.0, 0.0, 0.0, 0.0, 0.0] # params can't be negative
-ub = [Inf, Inf, Inf, Inf, Inf]
-# p0 = [1e8, 59, 3.46e-8, 2.0e-3, porosity, 0.0125] 
+ub = [Inf, Inf, Inf, Inf, Inf] 
 p_scaled = [16.8, 65.5, 0.59, 3.15, 0.0026]
 
-output1 = DAMM(Ind_var, p_scaled)
+SWC = []
+Tsoil = []
+Rsoil = []
+poro_vals = []
+fit = []
+Param_fit = []
+Modeled_data = []
+for i = 1:64
+	name_RS = string("RSM_Exp_Flux_", x[i], y[i])
+	name_SWC = string("SWC_", x[i], y[i])
+	name_Tsoil = string("Tsoil_", x[i], y[i])
 
-# include("AndiMD_fitf.jl") # function to scale model param
-
-# Fit, with scaling, model DAMM to data
-#res1 = curve_fit(DAMM, Ind_var, Dep_var, p_scaled, 
-#		 p->[1e7*p[1], p[2], 1e-8*p[3], 1e-3*p[4], p[5], 1e-2*p[6]],
-#		 p->[1e-7*p[1], p[2], 1e8*p[3], 1e3*p[4], p[5], 1e2*p[6]],
-#		 inplace=false, g_tol=1E-16, x_tol=1e-16, lower=lb, upper=ub)
-
-using LsqFit
-
-fit = curve_fit(DAMM, Ind_var, Dep_var, p_scaled, lower=lb, upper=ub)
-Param_fit = coef(fit) 
-
-Modeled_data = DAMM(Ind_var, Param_fit) 
+	d = dropmissing(data, name_RS)
+	d = dropmissing(d, name_SWC)
+	push!(SWC, d[!, name_SWC])
+	push!(Tsoil, d[!, name_Tsoil])
+	push!(Rsoil, d[!, name_RS])
+	Ind_var = hcat(Tsoil[i], SWC[i])
+	push!(poro_vals, maximum(skipmissing(data[!, name_SWC])))
+	poro_val = poro_vals[i]
+	push!(fit, curve_fit(DAMM, Ind_var, Rsoil[i], p_scaled, lower=lb, upper=ub))
+	push!(Param_fit, coef(fit[i]))
+	push!(Modeled_data, DAMM(Ind_var, Param_fit[i]))
+end
 
 # We can estimate errors on the fit parameters,
 # to get standard error of each parameter:
-sigma = stderror(fit)
+sigma = stderror(fit[1])
 # to get margin of error and confidence interval of each parameter at 5% significance level:
-margin_of_error = margin_error(fit, 0.05)
-confidence_inter = confidence_interval(fit, 0.05)
-
+margin_of_error = margin_error(fit[1], 0.05)
+confidence_inter = confidence_interval(fit[1], 0.05)
 # parameter covariance matrix evaluated at the best fit point
-covar = estimate_covar(fit)
-
+covar = estimate_covar(fit[1])
 # Calculate RMSE, sqrt of sum of square of residuals
-
 # correlation (r2) of param from covar --> see with Julie
-
 # v1 / sqrt(diag(v1)) %*% t(sqrt(diag(v1)))
-
 # s_ij/si*sj
 
 using GLMakie, UnicodeFun
 
+i = 63
+poro_val = poro_vals[i]
+
 fig = Figure()
 ax3D = Axis3(fig[1,1])
-p3D = scatter!(ax3D, Tsoil, SWC, Rsoil, markersize = 5000, color = :black)
-p3Dm = scatter!(ax3D, Tsoil, SWC, Modeled_data, markersize = 5000, color = :red)
+p3D = scatter!(ax3D, Tsoil[i], SWC[i], Rsoil[i], markersize = 5000, color = :black)
+p3Dm = scatter!(ax3D, Tsoil[i], SWC[i], Modeled_data[i], markersize = 5000, color = :red)
 
 
 L = 25 # resolution
 x = collect(range(1, length=L, stop=1))
 [append!(x, collect(range(i, length=L, stop=i))) for i = 2:25]
 x = reduce(vcat,x)
-y = collect(range(0, length=L, stop=poro_val))
+y = collect(range(0, length=L, stop=poro_vals[i]))
 y = repeat(y, outer=L)
 x_range = hcat(x, y)
 
 x = Int.(x_range[:, 1])
-y_ax = collect(range(0, length=L, stop=poro_val))
+y_ax = collect(range(0, length=L, stop=poro_vals[i]))
 y = collect(range(1, length=L, stop=L))
 y = repeat(y, outer=L)
 y = Int.(y)
 x_ax = collect(range(1, length=L, stop=L))
 
 using SparseArrays
-surface!(ax3D, x_ax, y_ax, Matrix(sparse(x, y, DAMM(x_range, Param_fit))), colormap = Reverse(:Spectral), transparency = true, alpha = 0.2, shading = false)
+surface!(ax3D, x_ax, y_ax, Matrix(sparse(x, y, DAMM(x_range, Param_fit[i]))), colormap = Reverse(:Spectral), transparency = true, alpha = 0.2, shading = false)
 
-wireframe!(ax3D, x_ax, y_ax, Matrix(sparse(x, y, DAMM(x_range, Param_fit))), overdraw = true, transparency = true, color = (:black, 0.1));
+wireframe!(ax3D, x_ax, y_ax, Matrix(sparse(x, y, DAMM(x_range, Param_fit[i]))), overdraw = true, transparency = true, color = (:black, 0.1));
 
 
 ax3D.xlabel = to_latex("T_{soil} (°C)");
